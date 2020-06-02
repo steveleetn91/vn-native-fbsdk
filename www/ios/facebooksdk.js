ec0da3e167289496afdcaf4cd3d55c3911d94ca6
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-06-03 03:03:25 +0700

    Setup Testing

--- www/ios/facebooksdk.js
+++ www/ios/facebooksdk.js
@@ -1,8 +1,6 @@
 var exec = require('cordova/exec');
 module.exports = {
     login : function(permission,success,fail) {
-        exec(success,fail, (err) => {
-            fail(err);
-        }, "FacebookSdk", "login", permission);
+        exec(success,fail,"FacebookSdk", "login", permission);
     }
 }
