3b6f3b8d34eb6dabd2811f4388338bce377177d2
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-06-06 16:09:31 +0700

    Update for testing

--- www/windows/facebooksdk.js
+++ www/windows/facebooksdk.js
@@ -1,20 +1,26 @@
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: appId,
-        cookie: true,
-        xfbml: true,
-        version: apiVersion
-    });
 
-    FB.AppEvents.logPageView();
-
+module.exports = {
+    setup : function(appId){
+        window.fbAsyncInit = function () {
+            FB.init({
+                appId: appId,
+                cookie: true,
+                xfbml: true,
+                version: 'v7.0'
+            });
+        
+            FB.AppEvents.logPageView();
+        
+        };
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    },
+    connect : function(){
+        return FB;
+    }
 };
-
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-module.exports = FB;
