d489264671a85c3fbba8fe26f331d2059f21a98b
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-06-06 15:43:24 +0700

    Update for testing

--- www/browser/facebooksdk.js
+++ www/browser/facebooksdk.js
@@ -1,11 +1,4 @@
 
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 module.exports = {
     setup : function(appId){
         window.fbAsyncInit = function () {
@@ -19,6 +12,13 @@ module.exports = {
             FB.AppEvents.logPageView();
         
         };
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     },
     connect : function(){
         setTimeout(() => {
