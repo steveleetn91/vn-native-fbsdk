af9008b615d8000eee1372ff9565437012b0312f
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-06-07 14:22:55 +0700

    Release

--- www/osx/facebooksdk.js
+++ www/osx/facebooksdk.js
@@ -1,26 +1,15 @@
 
 module.exports = {
     setup : function(appId){
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: appId,
-                cookie: true,
-                xfbml: true,
-                version: 'v7.0'
-            });
-        
-            FB.AppEvents.logPageView();
-        
-        };
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        return new Promise((resolve,reject) => {
+            return resolve(false);
+        });
     },
     connect : function(){
-        return FB;
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                return resolve(false);
+            },1000);
+        });
     }
 };
